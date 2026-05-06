1cf96683530916d63f24fbc6f5eb676c0ebdef7a
AliReza Zahedi <zahedi_alireza79@hotmail.com>
2026-05-06 12:03:46 -0400

    refactor: updated the main for the correct ID.

--- src/hand_teleop/controller/main.py
+++ src/hand_teleop/controller/main.py
@@ -3,7 +3,7 @@ from motor_controller import Motor
 from motor_id_search import MotorIDSearch
 
 if __name__ == "__main__":
-    PORT = "/dev/ttyACM0"
+    PORT = "/dev/tty.usbmodem5A4B0486761"
 
     # ── Open the serial bus ONCE ──
     bus = MotorBus(port=PORT)
