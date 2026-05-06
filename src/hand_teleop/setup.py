ef16dd5a6946b47cb0f9ea00c2a59f7b77772c82
AliReza Zahedi <zahedi_alireza79@hotmail.com>
2026-05-06 12:37:24 -0400

    feat: create setup.py

---
+++ src/hand_teleop/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'hand_teleop'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your@email.com',
+    description='Hand gesture teleoperation for robot arm control',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'gesture_node = nodes.gesture_node:main',
+            'arm_controller_node = nodes.arm_controller_node:main',
+        ],
+    },
+)
