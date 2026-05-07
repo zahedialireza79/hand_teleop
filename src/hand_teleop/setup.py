734b6f8408149ba557e00491987ee4509e471132
AliReza Zahedi <zahedi_alireza79@hotmail.com>
2026-05-07 11:37:10 -0400

    fix: added the landmark.task to the setup.py file for MediaPipe.

--- src/hand_teleop/setup.py
+++ src/hand_teleop/setup.py
@@ -7,9 +7,12 @@ setup(
     version='0.1.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+    ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    # include the mediapipe model file
+    (f'lib/python3.12/site-packages/{package_name}/gesture',
+        ['hand_teleop/gesture/hand_landmarker.task']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
